c789a75645c665b922907107786794d53cd0af89
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2023-07-14 14:57:35 +0100

    Added files generated from spring initializr

---
+++ src/test/java/com/structuralengineering/calculations/CalculationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.structuralengineering.calculations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
